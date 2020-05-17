9a735d1d54f91b63cc2aeff24e0c23dcc5ebedb9
khairmuhammad-ybh <39018414+khairmuhammad-ybh@users.noreply.github.com>
2020-05-17 22:23:05 +0800

    Added image picker

--- src/screens/compare.screen.js
+++ src/screens/compare.screen.js
@@ -1,13 +1,33 @@
 import React, {PureComponent} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 
+import ImagePicker from 'react-native-image-picker';
+
 // styles
 import commonStyles from '../styles/common.style';
 
 class CompareScreen extends PureComponent {
+  chooseImage = () => {
+    ImagePicker.showImagePicker(response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('user cancel image picker');
+      } else if (response.error) {
+        console.log('ImagePicker error: ', response.error);
+      } else {
+        const source = {uri: response.uri};
+        console.log('other: ', source);
+      }
+    });
+  };
+
   render() {
     return (
       <View style={commonStyles.container}>
+        <TouchableOpacity onPress={() => this.chooseImage()}>
+          <Text>Choose image</Text>
+        </TouchableOpacity>
       </View>
     );
   }
